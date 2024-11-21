73052d30939f107de92b365307c410f8af1990e4
Pedro Ribeiro <pedrorocha@gmail.com>
2024-11-21 17:46:00 -0300

    Resolvendo o ultimo bug!

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
 
+import { Provider } from 'react-redux'
+
 import { GlobalStyle } from './styles'
+
+import { store } from './store'
 
 export type Produto = {
   id: number
@@ -24,13 +28,13 @@ function App() {
   }
 
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header favoritos={favoritos} />
         <Produtos favoritos={favoritos} favoritar={favoritar} />
       </div>
-    </>
+    </Provider>
   )
 }
 
